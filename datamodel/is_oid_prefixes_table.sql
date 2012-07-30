bb8606c42ffb77d0f54e85876f936d32d0cef58d
Andreas Neumann <a.neumann@carto.net>
2012-07-30 14:20:00 +0200

    added table is_oid_prefixes containing one or more oid prefixes

---
+++ datamodel/is_oid_prefixes_table.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE qgep.is_oid_prefixes
+(
+  id serial NOT NULL,
+  prefix character(8),
+  active boolean,
+  CONSTRAINT pkey_qgep_is_oid_prefixes_id PRIMARY KEY (id )
+)
+WITH (
+  OIDS=FALSE
+);
+COMMENT ON TABLE qgep.is_oid_prefixes
+  IS 'This table contains OID prefixes for different communities or organizations. The application or administrator changing this table has to make sure that only one record is set to active.';
+
+CREATE INDEX in_qgep_is_oid_prefixes_active
+  ON qgep.is_oid_prefixes
+  USING btree
+  (active );
+
