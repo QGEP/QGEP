4c547a98767f2c9131a630448d054bf27feaf877
Andreas Neumann <a.neumann@carto.net>
2012-07-30 13:45:21 +0200

    changed generate_oid_function to use new prefix table is_oid_prefixes

--- datamodel/generate_oid_function.sql
+++ datamodel/generate_oid_function.sql
@@ -1,17 +1,25 @@
-﻿-- Function: qgep.generate_oid()
-
---DROP FUNCTION qgep.generate_oid();
-
-CREATE OR REPLACE FUNCTION qgep.generate_oid(prefix text, table_name text)
+﻿CREATE OR REPLACE FUNCTION qgep.generate_oid(table_name text)
   RETURNS text AS
 $BODY$
 DECLARE
+  myrec_prefix record;
   myrec_shortcut record;
   myrec_seq record;
 BEGIN
-  IF char_length(prefix) != 8 THEN
+  -- first we have to get the OID prefix
+  BEGIN
+    SELECT prefix::text INTO myrec_prefix FROM qgep.is_oid_prefixes WHERE active = TRUE;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+           RAISE EXCEPTION 'no active record found in table qgep.is_oid_prefixes';
+        WHEN TOO_MANY_ROWS THEN
+	   RAISE EXCEPTION 'more than one active records found in table qgep.is_oid_prefixes';
+  END;
+  -- test if prefix is of correct length
+  IF char_length(myrec_prefix.prefix) != 8 THEN
     RAISE EXCEPTION 'character length of prefix must be 8';
   END IF;
+  --get table 2char shortcut
   BEGIN
     SELECT shortcut_en INTO STRICT myrec_shortcut FROM qgep.is_dictionary WHERE tablename = table_name;
     EXCEPTION
@@ -20,11 +28,12 @@ BEGIN
         WHEN TOO_MANY_ROWS THEN
             RAISE EXCEPTION 'dictonary entry for table % not unique', table_name;
   END;
+  --get sequence for table
   SELECT nextval('qgep.seq_' || table_name || '_oid') AS seqval INTO myrec_seq;
   IF NOT FOUND THEN
     RAISE EXCEPTION 'sequence for table % not found', table_name;
   END IF;
-  RETURN prefix || myrec_shortcut.shortcut_en || to_char(myrec_seq.seqval,'FM000000');
+  RETURN myrec_prefix.prefix || myrec_shortcut.shortcut_en || to_char(myrec_seq.seqval,'FM000000');
 END;
 $BODY$
   LANGUAGE plpgsql STABLE
