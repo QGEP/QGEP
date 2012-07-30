258664fe8c40ac3e306fa701b499d4afb5f24804
Andreas Neumann <a.neumann@carto.net>
2012-07-30 17:34:55 +0200

    Added organization column to table is_oid_prefixes and one entry for Stadt Uster

--- datamodel/is_oid_prefixes_table.sql
+++ datamodel/is_oid_prefixes_table.sql
@@ -2,6 +2,7 @@
 (
   id serial NOT NULL,
   prefix character(8),
+  organization text,
   active boolean,
   CONSTRAINT pkey_qgep_is_oid_prefixes_id PRIMARY KEY (id )
 )
@@ -10,6 +11,8 @@ WITH (
 );
 COMMENT ON TABLE qgep.is_oid_prefixes
   IS 'This table contains OID prefixes for different communities or organizations. The application or administrator changing this table has to make sure that only one record is set to active.';
+
+INSERT INTO qgep.is_oid_prefixes (prefix,organization,active) VALUES ('ch11h8mw','Stadt Uster',TRUE);
 
 CREATE INDEX in_qgep_is_oid_prefixes_active
   ON qgep.is_oid_prefixes
