558388bbad2a30a55fd75a99ecb0ac78aaa4f738
Matthias Kuhn <matthias.kuhn@gmx.ch>
2012-11-09 15:45:43 +0100

    Add cover level to vw_network_node

--- datamodel/vw_network_node.sql
+++ datamodel/vw_network_node.sql
@@ -1,26 +1,34 @@
-﻿-- View: qgep.vw_reach_point
+﻿-- View: qgep.vw_network_node
 
--- DROP VIEW qgep.vw_reach_point;
+DROP VIEW qgep.vw_network_node;
 
 CREATE OR REPLACE VIEW qgep.vw_network_node AS 
  SELECT 
    row_number() OVER () AS gid,
    nodes.*
-   FROM
-   (
+ FROM
+ (
    SELECT 
-   obj_id,
-   'reach_point' AS type,
-   level AS level,
-   situation_geometry
+     obj_id,
+     'reach_point' AS type,
+     level AS level,
+     NULL AS cover_level,
+     NULL AS description,
+     situation_geometry
    FROM qgep.od_reach_point
-   
+ 
    UNION
-   
+ 
    SELECT 
-   obj_id,
-   'wastewater_node' AS type,
-   bottom_level AS level,
-   situation_geometry
-   FROM qgep.od_wastewater_node
-   ) AS nodes
+     wwne.obj_id,
+     'wastewater_node' AS type,
+     bottom_level AS level,
+     c.level AS cover_level,
+     wwne.identifier AS description,
+     wwno.situation_geometry
+   FROM qgep.od_wastewater_node wwno
+   LEFT JOIN qgep.od_wastewater_networkelement wwne
+   ON wwne.obj_id = wwno.obj_id
+   LEFT JOIN qgep.od_cover c
+   ON c.obj_id = wwno.obj_id
+ ) AS nodes
