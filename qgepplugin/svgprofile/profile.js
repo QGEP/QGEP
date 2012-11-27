3d7e5dd822aa5109e43fe01a33e18c5268d09792
Matthias Kuhn <matthias.kuhn@gmx.ch>
2012-11-27 16:25:23 +0100

    Add license information to js file

--- qgepplugin/svgprofile/profile.js
+++ qgepplugin/svgprofile/profile.js
@@ -1,6 +1,24 @@
 /**
- * Author: Matthias Kuhn
  * Date: 28.10.12
+ * Copyright(C) 2012 Matthias Kuhn 
+ * matthias dot kuhn (at) gmx dot ch
+ *
+ *
+ * licensed under the terms of GNU GPL 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this progsram; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 
