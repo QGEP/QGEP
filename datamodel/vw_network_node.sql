f67d98ca96fc65778d6aa3ceae542e3aa5d9e032
Matthias Kuhn <matthias.kuhn@gmx.ch>
2012-11-28 23:57:46 +0100

    add usage_current to special_structures

--- datamodel/vw_network_node.sql
+++ datamodel/vw_network_node.sql
@@ -12,6 +12,7 @@ CREATE OR REPLACE VIEW qgep.vw_network_node AS
      obj_id,
      'reach_point' AS type,
      level AS level,
+     NULL AS usage_current,
      NULL AS cover_level,
      NULL AS description,
      situation_geometry
@@ -23,12 +24,24 @@ CREATE OR REPLACE VIEW qgep.vw_network_node AS
      wwne.obj_id,
      'wastewater_node' AS type,
      bottom_level AS level,
+     COALESCE( MAX( ch_from.usage_current ), MAX( ch_to.usage_current ) ) AS usage_current,
      c.level AS cover_level,
      wwne.identifier AS description,
      wwno.situation_geometry
    FROM qgep.od_wastewater_node wwno
    LEFT JOIN qgep.od_wastewater_networkelement wwne
-   ON wwne.obj_id = wwno.obj_id
+     ON wwne.obj_id = wwno.obj_id
    LEFT JOIN qgep.od_cover c
-   ON c.obj_id = wwno.obj_id
- ) AS nodes
+     ON c.obj_id = wwno.obj_id
+   LEFT JOIN qgep.od_reach_point rp 
+     ON wwne.obj_id = rp.fs_wastewater_networkelement
+   LEFT JOIN qgep.od_reach re_from 
+     ON re_from.fs_reach_point_from = rp.obj_id
+   LEFT JOIN qgep.od_channel ch_from
+     ON ch_from.obj_id = re_from.obj_id
+   LEFT JOIN qgep.od_reach re_to 
+     ON re_to.fs_reach_point_to = rp.obj_id
+   LEFT JOIN qgep.od_channel ch_to
+     ON ch_to.obj_id = re_to.obj_id
+   GROUP BY wwne.obj_id, type, bottom_level, cover_level, description, wwno.situation_geometry
+  ) AS nodes
