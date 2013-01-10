37c4783af41d9c9c340a96b2f123d8a5e6d9de71
Matthias Kuhn <matthias.kuhn@gmx.ch>
2013-01-10 11:31:59 +0100

    Add french js translation file

---
+++ qgepplugin/i18n/qgepplugin-js_fr.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>reach</name>
+    <message>
+        <location filename="../svgprofile/profile/reach.js" line="60"/>
+        <source>Reach</source>
+        <comment>Reach</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/reach.js" line="61"/>
+        <source>Material:</source>
+        <comment>Material:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/reach.js" line="62"/>
+        <source>Width:</source>
+        <comment>Width:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/reach.js" line="63"/>
+        <source>Length:</source>
+        <comment>Length:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/reach.js" line="64"/>
+        <source>Gradient:</source>
+        <comment>Gradient:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/reach.js" line="65"/>
+        <source>Entry level:</source>
+        <comment>Entry level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/reach.js" line="66"/>
+        <source>Exit level:</source>
+        <comment>Exit level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>specialStructure</name>
+    <message>
+        <location filename="../svgprofile/profile/specialStructure.js" line="80"/>
+        <source>Manhole</source>
+        <comment>Manhole</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/specialStructure.js" line="91"/>
+        <source>Cover level:</source>
+        <comment>Cover level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/specialStructure.js" line="92"/>
+        <source>Bottom level:</source>
+        <comment>Bottom level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/specialStructure.js" line="93"/>
+        <source>Entry level:</source>
+        <comment>Entry level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../svgprofile/profile/specialStructure.js" line="94"/>
+        <source>Exit level:</source>
+        <comment>Exit level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
