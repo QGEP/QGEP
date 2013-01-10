a63a8b75727d9c021733fb3bb1cc6c8dc95fa690
Matthias Kuhn <matthias.kuhn@gmx.ch>
2013-01-10 09:11:52 +0100

    Add translation files for de and fr (Mostly untranslated)

---
+++ qgepplugin/i18n/qgepplugin_de.ts
@@ -0,0 +1,216 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="de" sourcelanguage="en">
+<context>
+    <name>DlgAbout</name>
+    <message>
+        <location filename="ui_dlgabout.py" line="82"/>
+        <source>Dialog</source>
+        <translation type="unfinished">Dialog</translation>
+    </message>
+    <message>
+        <location filename="ui_dlgabout.py" line="83"/>
+        <source>$PLUGIN_DESCRIPTION$</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dlgabout.py" line="84"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Cantarell&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-weight:600;&quot;&gt;QGEP plugin&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;This plugin helps to maintain wastewater structures in city areas. It is based on the VSA - DSS data structure.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;For any feedback, bugs or comments, please visit &lt;/span&gt;&lt;a href=&quot;https://github.com/qgep/QGEP/issues&quot;&gt;&lt;span style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/qgep/QGEP/issues.&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;If you find this plugin useful don&apos;t hesitate to let us know desired improvements!&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;QGEP plugin - License GNU GPL 2&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;Written 2012 by Matthias Kuhn&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:600;&quot;&gt;Requirements:&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;- networkx&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;- QGIS &amp;gt;=1.8&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:600;&quot;&gt;Sponsor:&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;City of Uster&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_dlgabout.py" line="104"/>
+        <source>QGEP</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QgepDockWidget</name>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="120"/>
+        <source>QGEP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="125"/>
+        <source>&amp;Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="121"/>
+        <source>Add reaches to selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="122"/>
+        <source>Perform calculation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="123"/>
+        <source>Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="124"/>
+        <source>10x</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="127"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_qgepdockwidget.py" line="126"/>
+        <source>Recreate graph</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QgepSettingsDialog</name>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="235"/>
+        <source>Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="240"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="245"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.py" line="127"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="30"/>
+        <source>Helper line color</source>
+        <translation>Farbe der Hilfslinie</translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="37"/>
+        <source>Current profile color</source>
+        <translation>Farbe des aktuellen Profils</translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="44"/>
+        <source>Feature highlighting color</source>
+        <translation>Farbe für das Hervorheben von Features</translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="24"/>
+        <source>Layout Options</source>
+        <translation>Aussehen</translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="127"/>
+        <source>Layers</source>
+        <translation>Ebenen</translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="133"/>
+        <source>Special structures:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="143"/>
+        <source>Graph nodes:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="156"/>
+        <source>Graph edges:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="115"/>
+        <source>Project Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="171"/>
+        <source>Enables developer tools on the profile widget. Reload and Inspect options are shown. The dock needs to be reopened.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="174"/>
+        <source>Developer mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="181"/>
+        <source>Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="190"/>
+        <source>Override default profile template</source>
+        <translation type="unfinished">Standartprofilvorlage überschreiben</translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="286"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="218"/>
+        <source>Logging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="227"/>
+        <source>Log level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="250"/>
+        <source>Critical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="258"/>
+        <source>Log to file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="267"/>
+        <source>Log file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgepsettingsdialog.ui" line="165"/>
+        <source>Developer Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
