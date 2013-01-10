469526a60894ff1082072e24ccfbc324766a74a6
Matthias Kuhn <matthias.kuhn@gmx.ch>
2013-01-10 11:26:52 +0100

    Add (untranslated) german javascript translation file

---
+++ qgepplugin/i18n/qgepplugin-js_de.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>reach</name>
+    <message>
+        <source>Reach</source>
+        <comment>Reach</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Material:</source>
+        <comment>Material:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Width:</source>
+        <comment>Width:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Length:</source>
+        <comment>Length:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gradient:</source>
+        <comment>Gradient:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Entry level:</source>
+        <comment>Entry level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit level:</source>
+        <comment>Exit level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>specialStructure</name>
+    <message>
+        <source>Manhole</source>
+        <comment>Manhole</comment>
+        <translation type="unfinished">Normschacht</translation>
+    </message>
+    <message>
+        <source>Cover level:</source>
+        <comment>Cover level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bottom level:</source>
+        <comment>Bottom level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Entry level:</source>
+        <comment>Entry level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit level:</source>
+        <comment>Exit level:</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
