c2d992e73518d1630765a482264cefff09669dc2
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-08-19 16:54:29 +0200

    Fix reach_point

--- project/migration/10_od_reach_point.sql
+++ project/migration/10_od_reach_point.sql
@@ -1,4 +1,4 @@
------- This file is sql code to import DABASE (Modul SIA405Abwasser) in German to QGEP in Englisch on QQIS
+﻿------ This file is sql code to import DABASE (Modul SIA405Abwasser) in German to QGEP in Englisch on QQIS
 ------ For questions etc. please contact Stefan Burckhardt stefan.burckhardt@sjib.ch
 ------ version 07.06.2014 22:00:52
 
@@ -10,17 +10,10 @@ ALTER TABLE qgep.od_reach_point ADD COLUMN old_obj_id text;
 -- 2. loeschen vorhandener daten
 DELETE FROM qgep.od_reach_point;
 
--- 3. update der oid spalten
--- In Tabelle "qgep.is_dictionary" muss für jede od_xx Tabelle ein Eintrag existieren und es muss im qgep-Schema für jede od_xx Tabelle eine Sequenz geben.
-SELECT setval('qgep.seq_od_reach_point_oid', 1, true);
-UPDATE qgep.od_wastewater_networkelement SET obj_id=qgep.generate_oid('od_reach_point')
-WHERE qgep.od_wastewater_networkelement.old_obj_id IN (SELECT obj_id FROM abwasser.siedlungsentwaesserung__Haltungspunkt);
-
 -- 4. einsetzen der daten von siedlungsentwaesserung in qgep.
 INSERT INTO qgep.od_reach_point
-(obj_id, old_obj_id, elevation_accuracy, identifier, level, outlet_shape, position_of_connection, remark, situation_geometry, last_modification, Dataowner) 
-SELECT q_wastewater_networkelement.obj_id, 
-a_haltepunkt.obj_id,
+(obj_id, elevation_accuracy, identifier, level, outlet_shape, position_of_connection, remark, situation_geometry, last_modification, Dataowner) 
+SELECT obj_id,
 CASE WHEN hoehengenauigkeit = 0 THEN 3248 --- more_than_6cm
 WHEN hoehengenauigkeit = 1 THEN 3245 --- plusminus_1cm
 WHEN hoehengenauigkeit = 2 THEN 3246 --- plusminus_3cm
@@ -37,6 +30,10 @@ END,
 lage_anschluss, bemerkung, the_geom, 
 to_timestamp (LETZTE_AENDERUNG, 'DD MM YYYY'),
 MD_DATENHERR
-FROM abwasser.siedlungsentwaesserung__Haltungspunkt a_haltepunkt
-LEFT JOIN qgep.od_wastewater_networkelement q_wastewater_networkelement
-ON a_haltepunkt.obj_id = q_wastewater_networkelement.old_obj_id;
+FROM abwasser.siedlungsentwaesserung__Haltungspunkt;
+
+-- 3. update der oid spalten
+-- In Tabelle "qgep.is_dictionary" muss für jede od_xx Tabelle ein Eintrag existieren und es muss im qgep-Schema für jede od_xx Tabelle eine Sequenz geben.
+UPDATE qgep.od_reach_point SET old_obj_id=obj_id;
+SELECT setval('qgep.seq_od_reach_point_oid', 1, true);
+UPDATE qgep.od_reach_point SET obj_id=qgep.generate_oid('od_reach_point');
