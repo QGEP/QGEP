ac3c33c14d6ffe283e9148aec1a2ddfd55a0958b
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-08-27 10:14:43 +0200

    Add od_catchment_area import sql

---
+++ migration/27_od_catchment_area.sql
@@ -0,0 +1,65 @@
+﻿------ 26.0814: for development only, uses table ezg in schema public
+------ This file is sql code to import DABASE (Modul SIA405Abwasser) in German to QGEP in Englisch on QQIS
+------ For questions etc. please contact Stefan Burckhardt stefan.burckhardt@sjib.ch
+------ version 07.06.2014 22:00:52
+
+-- 1. ergaenzen tabelle mit oid spalte
+ALTER TABLE qgep.od_catchment_area DROP COLUMN IF EXISTS old_obj_id;
+ALTER TABLE qgep.od_catchment_area ADD COLUMN old_obj_id text;
+
+-------
+-- 2. loeschen vorhandener daten
+DELETE FROM qgep.od_catchment_area;
+
+-- 3. einsetzen der daten von siedlungsentwaesserung in qgep.
+INSERT INTO qgep.od_catchment_area
+(obj_id,
+-- direct_discharge_current,
+-- direct_discharge_planned,
+discharge_coefficient_rw_current,
+-- discharge_coefficient_rw_planned,
+-- discharge_coefficient_ww_current,
+-- discharge_coefficient_ww_planned,
+--- drainage_system_current,
+-- drainage_system_planned,
+identifier,
+-- infiltration_current,
+-- infiltration_planned,
+perimeter_geometry,
+-- population_density_current,
+-- population_density_planned,
+remark,
+-- retention_current,
+-- retention_planned,
+-- runoff_limit_current,
+-- runoff_limit_planned,
+-- seal_factor_rw_current,
+-- seal_factor_rw_planned,
+-- seal_factor_ww_current,
+-- seal_factor_ww_planned,
+-- sewer_infiltration_water_production_current,
+-- sewer_infiltration_water_production_planned,
+surface_area,
+-- waste_water_production_current,
+-- waste_water_production_planned,
+-- last_modification,
+-- dataowner
+fk_wastewater_networkelement_rw_current
+)
+SELECT
+a_ezg.obj_id,
+abflussbei,
+--- art,
+bezeichnun,
+ST_GeometryN( the_geom, 1 ),
+bemerkung,
+flaeche,
+q_wastewater_networkelement.obj_id
+FROM abwasser.siedlungsentwaesserung__einzugsgebiet a_ezg
+LEFT JOIN qgep.od_wastewater_networkelement q_wastewater_networkelement
+on a_ezg.abwasserne = q_wastewater_networkelement.old_obj_id;
+
+
+
+-- 4. update der oid spalten
+UPDATE qgep.od_catchment_area SET old_obj_id=obj_id;
