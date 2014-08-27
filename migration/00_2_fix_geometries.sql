b17de320d5f05a8c9716bddde110a3d46ce03bc9
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-08-27 21:22:05 +0200

    Fix od_reach_point.fk_wastewater_networkelement

---
+++ migration/00_2_fix_geometries.sql
@@ -0,0 +1,4 @@
+﻿UPDATE abwasser.siedlungsentwaesserung__abwasserbauwerk AS awb
+SET the_geom = awbg.the_geom
+FROM abwasser.od_wastewater_structure AS awbg
+WHERE awbg.obj_id = regexp_replace(awb.obj_id, ' ', '_');
--- migration/99_foreign_keys.sql
+++ migration/99_foreign_keys.sql
@@ -1,8 +1,6 @@
 ﻿UPDATE qgep.od_reach_point
 SET fk_wastewater_networkelement = q_wastewater_networkelement.obj_id
-FROM 
-qgep.od_reach_point AS q_reach_point
-LEFT JOIN abwasser.siedlungsentwaesserung__Haltungspunkt AS a_haltungspunkt
-ON a_haltungspunkt.obj_id = q_reach_point.old_obj_id
-LEFT JOIN qgep.od_wastewater_networkelement AS q_wastewater_networkelement
-ON a_haltungspunkt.abwassernetzelement = q_wastewater_networkelement.old_obj_id;
+FROM abwasser.siedlungsentwaesserung__Haltungspunkt AS a_haltungspunkt
+  LEFT JOIN qgep.od_wastewater_networkelement AS q_wastewater_networkelement
+  ON a_haltungspunkt.abwassernetzelement = q_wastewater_networkelement.old_obj_id
+WHERE a_haltungspunkt.obj_id = qgep.od_reach_point.old_obj_id;
