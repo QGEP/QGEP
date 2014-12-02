a49a0a04020a06afeacc9e90df7c341ad394a19d
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-12-02 18:14:16 +0100

    Revert "Update 04_od_manhole.sql"
    This reverts commit eb56e7bc52986fffaa85d485dddc70d6e95c3d5c.

--- migration/04_od_manhole.sql
+++ migration/04_od_manhole.sql
@@ -34,16 +34,15 @@ WHEN funktion = 3 THEN 3267 --- rain_water_manhole
 WHEN funktion = 4 THEN 3266 --- gully
 WHEN funktion = 5 THEN 3472 --- drainage_channel
 WHEN funktion = 6 THEN 228 --- rail_track_gully
-WHEN funktion = 7 THEN 5344 --- 'Hochwasserentlastung' --> other
-WHEN funktion = 8 THEN 5344 --- 'Klärgrube' --> other
-WHEN funktion = 9 THEN 204 --- manhole
-WHEN funktion = 10 THEN 1008 --- oil_separator
-WHEN funktion = 11 THEN 4536 --- pump_station
-WHEN funktion = 12 THEN 2742 --- slurry_collector
-WHEN funktion = 13 THEN 5347 --- floating_material_separator
-WHEN funktion = 14 THEN 4537 --- jetting_manhole
-WHEN funktion = 15 THEN 4798 --- separating_structure
-WHEN funktion = 16 THEN 5345 --- unknown
+WHEN funktion = 7 THEN 204 --- manhole
+WHEN funktion = 8 THEN 1008 --- oil_separator
+WHEN funktion = 9 THEN 4536 --- pump_station
+WHEN funktion = 10 THEN 5346 --- stormwater_overflow
+WHEN funktion = 11 THEN 2742 --- slurry_collector
+WHEN funktion = 12 THEN 5347 --- floating_material_separator
+WHEN funktion = 13 THEN 4537 --- jetting_manhole
+WHEN funktion = 14 THEN 4798 --- separating_structure
+WHEN funktion = 15 THEN 5345 --- unknown
 END AS function,
 CASE 
 WHEN material = 0 THEN 4540 --- other
