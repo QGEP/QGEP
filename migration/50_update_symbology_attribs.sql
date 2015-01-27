839ac499b48bcaecc12f909e019fa86227e3866d
Andreas Neumann <a.neumann@carto.net>
2015-01-27 15:47:08 +0100

    added symbology update script to migration script to update manhole symbology attributes

---
+++ migration/50_update_symbology_attribs.sql
@@ -0,0 +1,8 @@
+﻿-- this post-processing script updates non-standard attributes necessary for symbology - e.g. for manholes
+-- update _usage_current and _function_hierarchic for manholes
+UPDATE qgep.od_manhole
+   SET _usage_current=(SELECT usage_current FROM qgep.manhole_symbology_attribs(obj_id)),
+       _function_hierarchic=(SELECT function_hierarchic FROM qgep.manhole_symbology_attribs(obj_id))
+;
+-- set manhole orientation to zero where it isn't already set
+UPDATE qgep.od_manhole SET _orientation = 0 WHERE _orientation IS NULL;
