12430f8ab0bb88103b6aa2bc3a543ec54e882c9f
Denis Rouzaud <denis.rouzaud@gmail.com>
2015-03-24 15:01:02 +0100

    fix scripts names

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 psql "service=pg_qgep user=postgres" < ../datamodel/00_qgep_schema.sql
 psql "service=pg_qgep user=postgres" < ../datamodel/01_audit.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/02_qgep_meta_dss.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/03_oid_generation.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/04_qgep_db_dss.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/05_vsa_kek_extension.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/06_data_model_extensions.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/07_symbology_functions.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/08_views_for_network_tracking.sql
-psql "service=pg_qgep user=postgres" < ../datamodel/09_qgep_functions.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/02_oid_generation.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/03_qgep_db_dss.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/04_vsa_kek_extension.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/05_data_model_extensions.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/06_symbology_functions.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/07_views_for_network_tracking.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/08_qgep_functions.sql
+psql "service=pg_qgep user=postgres" < ../datamodel/09_qgep_dictionaries.sql
 psql "service=pg_qgep user=postgres" < ../datamodel/view/vw_access_aid.sql
 psql "service=pg_qgep user=postgres" < ../datamodel/view/vw_benching.sql
 psql "service=pg_qgep user=postgres" < ../datamodel/view/vw_channel.sql
@@ -23,4 +23,3 @@ psql "service=pg_qgep user=postgres" < ../datamodel/view/vw_wastewater_node.sql
 psql "service=pg_qgep user=postgres" < ../datamodel/view/vw_wizard_cover_manhole.sql
 psql "service=pg_qgep user=postgres" < ../datamodel/view/vw_wizard_cover_special_structure.sql
 psql "service=pg_qgep user=postgres" < ../datamodel/view/vw_wizard_reach_channel.sql
-
