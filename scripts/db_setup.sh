0c5e975e57af9bff2624be73cd717c04ed1599bc
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-03-26 08:14:25 +0100

    Let database setup script to exit on error
    And write some explanations into its header

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
-if [ "0$PGSERVICE" = "0" ]
+
+# This script will create a clean datastructure for the QGEP project based on
+# the SIA 405 datamodel.
+# It will create a new schema qgep in the postgres database.
+#
+# Environment variables:
+#
+#  * PGSERVICE
+#      Specifies the postgres database to be used
+#      Examples:
+#        export PGSERVICE=pg_qgep
+#        export PGSERVICE="pg_qgep user=postgres"
+
+# Exit on error
+set -e
+
+if [ "0${PGSERVICE}" = "0" ]
 then
   PGSERVICE=pg_qgep
 fi
 
-echo "" > .~qgep_import.sql
-
-cat ../datamodel/00_qgep_schema.sql                         >> .~qgep_import.sql
-cat ../datamodel/01_audit.sql                               >> .~qgep_import.sql
-cat ../datamodel/02_oid_generation.sql                      >> .~qgep_import.sql
-cat ../datamodel/03_qgep_db_dss.sql                         >> .~qgep_import.sql
-cat ../datamodel/04_vsa_kek_extension.sql                   >> .~qgep_import.sql
-cat ../datamodel/05_data_model_extensions.sql               >> .~qgep_import.sql
-cat ../datamodel/06_symbology_functions.sql                 >> .~qgep_import.sql
-cat ../datamodel/07_views_for_network_tracking.sql          >> .~qgep_import.sql
-cat ../datamodel/08_qgep_functions.sql                      >> .~qgep_import.sql
-cat ../datamodel/09_qgep_dictionaries.sql                   >> .~qgep_import.sql
-cat ../datamodel/view/vw_access_aid.sql                     >> .~qgep_import.sql
-cat ../datamodel/view/vw_benching.sql                       >> .~qgep_import.sql
-cat ../datamodel/view/vw_channel.sql                        >> .~qgep_import.sql
-cat ../datamodel/view/vw_cover.sql                          >> .~qgep_import.sql
-cat ../datamodel/view/vw_discharge_point.sql                >> .~qgep_import.sql
-cat ../datamodel/view/vw_dryweather_downspout.sql           >> .~qgep_import.sql
-cat ../datamodel/view/vw_dryweather_flume.sql               >> .~qgep_import.sql
-cat ../datamodel/view/vw_manhole.sql                        >> .~qgep_import.sql
-cat ../datamodel/view/vw_reach.sql                          >> .~qgep_import.sql
-cat ../datamodel/view/vw_special_structure.sql              >> .~qgep_import.sql
-cat ../datamodel/view/vw_wastewater_node.sql                >> .~qgep_import.sql
-cat ../datamodel/view/vw_wizard_cover_manhole.sql           >> .~qgep_import.sql
-cat ../datamodel/view/vw_wizard_cover_special_structure.sql >> .~qgep_import.sql
-cat ../datamodel/view/vw_wizard_reach_channel.sql           >> .~qgep_import.sql
-
-sed -i 's/\xEF\xBB\xBF//' .~qgep_import.sql
-
-psql "service=$PGSERVICE" -v ON_ERROR_STOP=1 -f .~qgep_import.sql
-
-rm .~qgep_import.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/00_qgep_schema.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/01_audit.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/02_oid_generation.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/03_qgep_db_dss.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/04_vsa_kek_extension.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/05_data_model_extensions.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/06_symbology_functions.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/07_views_for_network_tracking.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/08_qgep_functions.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/09_qgep_dictionaries.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_access_aid.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_benching.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_channel.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_cover.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_discharge_point.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_dryweather_downspout.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_dryweather_flume.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_manhole.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_reach.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_special_structure.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_wastewater_node.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_wizard_cover_manhole.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_wizard_cover_special_structure.sql
+psql "service=${PGSERVICE}" -v ON_ERROR_STOP=1 -f ../datamodel/view/vw_wizard_reach_channel.sql
