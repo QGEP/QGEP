b2e588e922ef1528411f0a4f1f6cadd90b24259a
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-04-22 10:44:13 +0200

    More view translations

--- datamodel/10_dict_extension.sql
+++ datamodel/10_dict_extension.sql
@@ -55,38 +55,6 @@ INSERT INTO qgep.is_dictionary_vw_field (
   WHERE table_name = 'od_reach'
   OR table_name = 'od_wastewater_networkelement';
 
--- Cover
-
-INSERT INTO qgep.is_dictionary_vw_field ( 
-  class_id, 
-  attribute_id, 
-  table_name, 
-  field_name, 
-  field_name_en, 
-  field_name_de, 
-  field_name_fr, 
-  field_name_it, 
-  field_description_en, 
-  field_description_de, 
-  field_description_fr, 
-  field_description_it 
-) SELECT 
-    class_id,
-    attribute_id, 
-    'vw_cover', 
-    field_name, 
-    field_name_en, 
-    field_name_de, 
-    field_name_fr, 
-    field_name_it, 
-    field_description_en, 
-    field_description_de, 
-    field_description_fr, 
-    field_description_it 
-  FROM qgep.is_dictionary_od_field 
-  WHERE table_name = 'od_cover'
-  OR table_name = 'od_structure_part';
-
 -- Manhole
 
 INSERT INTO qgep.is_dictionary_vw_field ( 
@@ -151,6 +119,272 @@ INSERT INTO qgep.is_dictionary_vw_field (
   FROM qgep.is_dictionary_od_field 
   WHERE table_name = 'od_special_structure'
   OR table_name = 'od_wastewater_structure';
+
+
+-- Special Structure
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_channel', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_channel'
+  OR table_name = 'od_wastewater_structure';
+
+-- Discharge Point
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_discharge_point', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_discharge_point'
+  OR table_name = 'od_wastewater_structure';
+
+  
+-- Wastewater Node
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_wastewater_node', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_wastewater_node'
+  OR table_name = 'od_wastewater_networkelement';
+
+
+-- Cover
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_cover', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_cover'
+  OR table_name = 'od_structure_part';
+
+
+-- Access Aid
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_cover', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_access_aid'
+  OR table_name = 'od_structure_part';
+
+
+-- Benching
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_benching', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_benching'
+  OR table_name = 'od_structure_part';
+
+
+-- Dryweather Downspout
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_dryweather_downspout', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_dryweather_downspout'
+  OR table_name = 'od_structure_part';
+
+
+-- Dryweather Flume
+
+INSERT INTO qgep.is_dictionary_vw_field ( 
+  class_id, 
+  attribute_id, 
+  table_name, 
+  field_name, 
+  field_name_en, 
+  field_name_de, 
+  field_name_fr, 
+  field_name_it, 
+  field_description_en, 
+  field_description_de, 
+  field_description_fr, 
+  field_description_it 
+) SELECT 
+    class_id,
+    attribute_id, 
+    'vw_dryweather_flume', 
+    field_name, 
+    field_name_en, 
+    field_name_de, 
+    field_name_fr, 
+    field_name_it, 
+    field_description_en, 
+    field_description_de, 
+    field_description_fr, 
+    field_description_it 
+  FROM qgep.is_dictionary_od_field 
+  WHERE table_name = 'od_dryweather_flume'
+  OR table_name = 'od_structure_part';
+
+
+
 
 CREATE VIEW qgep.vw_dictionary_field AS
   SELECT * 
