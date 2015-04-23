f4311bde6585640455d307a2afea2323079f0066
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-04-23 09:11:29 +0200

    Project translation: don't translate obj_id and use table names as base

--- scripts/translation_encode_field_names.py
+++ scripts/translation_encode_field_names.py
@@ -1,4 +1,9 @@
 # The following commands can be executed in the python console.
 # Create field names in the format %#{table}#{field}#name#% for every layer in
 # the project
-[ [ l.addAttributeAlias( l.fieldNameIndex( f.name() ), '%#' + QgsDataSourceURI( l.dataProvider().dataSourceUri() ).table() + '#' + f.name() + '#name#%' ) for f in l.pendingFields()] for l in QgsMapLayerRegistry.instance().mapLayers().values() ]
+for l in QgsMapLayerRegistry.instance().mapLayers().values():
+    for f in l.pendingFields():
+        l.remAttributeAlias( l.fieldNameIndex( f.name() ) )
+        tbl_name = QgsDataSourceURI( l.dataProvider().dataSourceUri() ).table()
+        if (tbl_name[:3] == 'od_' or tbl_name[:3] == 'vw_') and f.name() != 'obj_id':
+            l.addAttributeAlias( l.fieldNameIndex( f.name() ), '%#' + tbl_name + '#' + f.name() + '#name#%' )
