3b9efb4f14c8a3eaead66051ba08a173ea7f43ee
Matthias Kuhn <matthias.kuhn@gmx.ch>
2015-05-26 23:33:10 +0200

    Add database translation sync script

--- qgepplugin/scripts/sync_translations.py
+++ qgepplugin/scripts/sync_translations.py
@@ -1,48 +1,32 @@
+# -*- coding: utf-8 -*-
+
 import psycopg2 as pg
 import xml.etree.ElementTree as ET
 import os
+import json
+import io
+import codecs
+from pprint import pprint
 
-pg.extensions.register_type(pg.extensions.UNICODE)
+basepath = os.path.dirname(os.path.realpath(__file__))
 
-views = {
-    'vw_cover': {
-        'parents': [
-            'od_cover',
-            'od_structure_part'
-        ]
-    },
-    'vw_access_aid': {
-        'parents': [
-            'od_access_aid',
-            'od_structure_part'
-        ]
-    },
-    'vw_dryweather_flume': {
-        'parents': [
-            'od_dryweather_flume',
-            'od_structure_part'
-        ]
-    },
-    'vw_dryweather_downspout': {
-        'parents': [
-            'od_dryweather_downspout',
-            'od_structure_part'
-        ]
-    },
-    'vw_benching': {
-        'parents': [
-            'od_benching',
-            'od_structure_part'
-        ]
-    }
-}
+with open(os.path.join(basepath, '../../datamodel/defs.json')) as defs_file:
+    views = json.load(defs_file)
 
+def set_translation(message, text):
 
-def syncLanguage(lang_code):
+    if text and text[:4] != 'zzz_':
+        print u'{} {} {}'.format(text, text[:4], text[:4] == 'zzz_')
+        message.find('translation').text = text
+        message.find('translation').attrib.pop('type')
+
+def sync_language(lang_code):
     fields = {}
 
     with pg.connect("service=pg_qgep") as conn:
         with conn.cursor() as cur:
+            pg.extensions.register_type(pg.extensions.UNICODE, cur)
+
             cur.execute('SELECT * FROM qgep.is_dictionary_od_field;')
 
             cols = [desc[0] for desc in cur.description]
@@ -56,7 +40,7 @@ def syncLanguage(lang_code):
                 fields[record[3]][record[4]]['name'] = record[nameidx]
                 fields[record[3]][record[4]]['description'] = record[descidx]
 
-    tsfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../i18n/qgep-project_{}.ts').format(
+    tsfile = os.path.join(basepath, '../i18n/qgep-project_{}.ts').format(
         lang_code)
     tree = ET.parse(tsfile)
     root = tree.getroot()
@@ -64,33 +48,41 @@ def syncLanguage(lang_code):
     for context in root.findall('context'):
         contextname = context.find('name').text
         layer = contextname[4:]
-        type = contextname[:3]
+        ltype = contextname[:3]
 
-        if type == 'fld':
+        if ltype == 'fld':
             for message in context.findall('message'):
                 # The source is the fieldname
                 source = message.find('source')
                 try:
-                    message.find('translation').text = fields[layer][source.text]['name']
-                    print 'Translated {} {} to {}'.format(layer, source.text, message.find('translation').text)
+                    # The most simple type: the translation is in the db for an od_ table
+                    set_translation(message, fields[layer][source.text]['name'])
                 except KeyError:
+                    # If there is no layer with that name it's probably a vw_ view... Lookup in the view definitions
                     if layer in views:
-                        for lyr in views[layer]['parents']:
+                        for lyr, opts in views[layer]['parents'].iteritems():
                             try:
-                                if fields[lyr][source.text]['name'] is not None:
-                                    message.find('translation').text = fields[lyr][source.text]['name']
-                                    message.find('translation').attrib.pop('type')
-                                    if source.text == 'last_modification':
-                                        print 'Translated {} {} to {}'.format(layer, source.text,
-                                                                              message.find('translation').text)
+                                src = source.text
+                                # If the field has a different name in the view, do the remapping
+                                if 'remap' in opts:
+                                    try:
+                                        src = opts['remap'][src]
+                                    except KeyError:
+                                        pass
+
+                                # Now search for the translation (remapped or not)
+                                set_translation(message, fields[lyr][src]['name'])
                             except KeyError:
                                 pass
 
-    with open(tsfile, 'w') as f:
-        f.write('<?xml version="1.0" ?><!DOCTYPE TS>')
-        tree.write(f, 'utf-8')
+    with codecs.open(tsfile, 'w', encoding='utf8') as f:
+        f.write(u'<?xml version="1.0" ?><!DOCTYPE TS>')
+        tree.write(f)
 
 
 for lang in ['fr', 'de', 'it', 'en']:
     print 'Translating {}'.format(lang)
-    syncLanguage(lang)
+    sync_language(lang)
+
+# Uncomment to debug views definitions:
+# pprint(views)
