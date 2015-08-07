5679f919a76c438dacf16eb7fdcca07d30ba77c9
Matthias Kuhn <matthias@opengis.ch>
2015-08-07 09:39:01 +0200

    Move qgepplugin to separate git repository https://github.com/QGEP/qgepplugin

--- qgepplugin/scripts/update-strings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-FILES=`find . -regex ".*\(\.ui\|\.py\)$"`
-pylupdate4 -noobsolete $FILES -ts i18n/qgepplugin_en.ts
-lupdate-qt4 svgprofile -recursive -no-obsolete -extensions js -ts i18n/qgepplugin-js_en.ts
-
