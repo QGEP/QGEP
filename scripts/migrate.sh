c0026ccb732baed952984d9f47097058be18206d
Matthias Kuhn <matthias@opengis.ch>
2015-08-14 14:23:28 +0200

    Don't require to run migrate.sh from the scripts directory

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
-# psql "service=pg_qgep user=postgres" < ../migration/00_1_copy_data
-# psql "service=pg_qgep user=postgres" < ../migration/00_2_fix_geometries.sql
-psql "service=pg_qgep user=postgres" < ../migration/01_od_organisation.sql
-echo "*** ws ***"
-psql "service=pg_qgep user=postgres" < ../migration/02_od_wastewater_structure.sql
-echo "*** ch ***"
-psql "service=pg_qgep user=postgres" < ../migration/03_od_channel.sql
-echo "*** mh ***"
-psql "service=pg_qgep user=postgres" < ../migration/04_od_manhole.sql
-echo "*** dp ***"
-psql "service=pg_qgep user=postgres" < ../migration/05_od_discharge_point.sql
-echo "*** ss ***"
-psql "service=pg_qgep user=postgres" < ../migration/06_od_special_structure.sql
-echo "*** pp ***"
-psql "service=pg_qgep user=postgres" < ../migration/08_od_pipe_profile.sql
-echo "*** ne ***"
-psql "service=pg_qgep user=postgres" < ../migration/09_od_wastewater_networkelement.sql
-echo "*** rp ***"
-psql "service=pg_qgep user=postgres" < ../migration/10_od_reach_point.sql
-echo "*** wn ***"
-psql "service=pg_qgep user=postgres" < ../migration/11_od_wastewater_node.sql
-echo "*** re ***"
-psql "service=pg_qgep user=postgres" < ../migration/12_od_reach.sql
-echo "*** sp ***"
-psql "service=pg_qgep user=postgres" < ../migration/13_od_structure_part.sql
-echo "*** aa ***"
-psql "service=pg_qgep user=postgres" < ../migration/14_od_access_aid.sql
-echo "*** co ***"
-psql "service=pg_qgep user=postgres" < ../migration/17_od_cover.sql
-echo "*** ca ***"
-psql "service=pg_qgep user=postgres" < ../migration/27_od_catchment_area.sql
-echo "*** fk ***"
-psql "service=pg_qgep user=postgres" < ../migration/50_foreign_keys.sql
-echo "*** fixup ***"
-psql "service=pg_qgep user=postgres" < ../migration/fixup/function_remap.sql
-echo "*** sym ***"
-psql "service=pg_qgep user=postgres" < ../migration/90_update_symbology_attribs.sql
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+
+# psql "service=pg_qgep user=postgres" < ${DIR}/migration/00_1_copy_data
+# psql "service=pg_qgep user=postgres" < ${DIR}/migration/00_2_fix_geometries.sql
+echo "*** Migrate 01_od_organisation ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/01_od_organisation.sql
+echo "*** Migrate 02_od_wastewater_structure (08) ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration08/02_od_wastewater_structure.sql
+echo "*** Migrate 03_od_channel (08) ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration08/03_od_channel.sql
+echo "*** Migrate 04_od_manhole (08) ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration08/04_od_manhole.sql
+echo "*** Migrate 05_od_discharge_point (08) ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration08/05_od_discharge_point.sql
+echo "*** Migrate ss 06_od_special_structure (08) ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration08/06_od_special_structure.sql
+echo "*** Migrate 08_od_pipe_profile ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/08_od_pipe_profile.sql
+echo "*** Migrate 09_od_wastewater_networkelement ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/09_od_wastewater_networkelement.sql
+echo "*** Migrate 10_od_reach_point ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/10_od_reach_point.sql
+echo "*** Migrate 11_od_wastewater_node ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/11_od_wastewater_node.sql
+echo "*** Migrate 12_od_reach ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/12_od_reach.sql
+echo "*** Migrate 13_od_structure_part ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/13_od_structure_part.sql
+echo "*** Migrate 14_od_access_aid ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/14_od_access_aid.sql
+echo "*** Migrate 17_od_cover ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/17_od_cover.sql
+echo "*** Migrate 27_od_catchment_area (08) ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration08/27_od_catchment_area.sql
+echo "*** Migrate 50_foreign_keys ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/50_foreign_keys.sql
+echo "*** Migrate 90_update_symbology_attribs ***"
+psql "service=pg_qgep user=postgres" < ${DIR}/migration/90_update_symbology_attribs.sql
+
