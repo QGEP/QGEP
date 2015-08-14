5b885e866f5eeb121e58a8875cecee87639cf3b6
Matthias Kuhn <matthias@opengis.ch>
2015-08-15 00:00:39 +0200

    Travis branch

--- scripts/ci/setup_git.sh
+++ scripts/ci/setup_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
+if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "travis";
 then
   openssl aes-256-cbc -K $encrypted_a8407ccc2176_key -iv $encrypted_a8407ccc2176_iv -in qgep_rsa.enc -out ~/.ssh/id_rsa -d
   chmod 600 ~/.ssh/id_rsa;
