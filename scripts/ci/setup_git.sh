fc001d269b21d53976e1d96c675dc452acee90c2
Matthias Kuhn <matthias@opengis.ch>
2016-01-18 15:35:34 +0100

    Use https instead of git (ssh) protocol when travis testing

--- scripts/ci/setup_git.sh
+++ scripts/ci/setup_git.sh
@@ -7,4 +7,12 @@ then
   git config --global user.email "qgep@opengis.ch";
   git config --global user.name "QGEP";
   git config --global push.default simple;
+else
+  sed -i 's|git@github.com:|https://github.com/|' .gitmodules
 fi
+
+git submodule update --init
+
+sed -i 's|git@github.com:|https://github.com/|' datamodel/.gitmodules
+
+git submodule update --init --recursive
