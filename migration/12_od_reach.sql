f1ae17d1b4993c7039e5c46ea31d36abadf6c50d
Matthias Kuhn <matthias@opengis.ch>
2016-07-20 08:32:49 +0200

    Update migration scripts for curved geometries

--- migration/12_od_reach.sql
+++ migration/12_od_reach.sql
@@ -21,7 +21,7 @@ INSERT INTO qgep.od_reach
 (obj_id, old_obj_id, clear_height, coefficient_of_friction, 
 -- elevation_determination, 
 horizontal_positioning, inside_coating, length_effective, material, 
-progression_geometry, 
+progression_geometry,
 -- progression_3d_geometry, reliner_material, reliner_nominal_size, relining_construction, relining_kind, ring_stiffness, slope_building_plan, 
 wall_roughness, fk_reach_point_from, fk_reach_point_to, fk_pipe_profile)
 SELECT 
@@ -71,7 +71,7 @@ WHEN material = 21 THEN 2761 --- clay
 WHEN material = 23 THEN 2762 --- cement
 ELSE 3016 --- unknown
 END,
-ST_RemoveRepeatedPoints( ST_GeometryN( a_haltung.the_geom, 1 ) ), -- Convert single-part multilinestring to linestring by extracting the first (and only) part
+ST_ForceCurve( ST_RemoveRepeatedPoints( ST_GeometryN( a_haltung.the_geom, 1 ) ) ), -- Convert single-part multilinestring to linestring by extracting the first (and only) part
 -- verlauf, 
 -- the_geom3D,  
 -- CASE WHEN reliner_material = 0 THEN 6459 --- other WHEN reliner_material = 1 THEN 6461 --- epoxy_resin_glass_fibre_laminate WHEN reliner_material = 2 THEN 6460 --- epoxy_resin_plastic_felt WHEN reliner_material = 3 THEN 6483 --- GUP_pipe WHEN reliner_material = 4 THEN 6462 --- HDPE WHEN reliner_material = 5 THEN 6484 --- isocyanate_resin_glass_fibre_laminate WHEN reliner_material = 6 THEN 6485 --- isocyanate_resin_plastic_felt WHEN reliner_material = 7 THEN 6464 --- polyester_resin_glass_fibre_laminate WHEN reliner_material = 8 THEN 6463 --- polyester_resin_plastic_felt WHEN reliner_material = 9 THEN 6482 --- polypropylene WHEN reliner_material = 10 THEN 6465 --- PVC WHEN reliner_material = 11 THEN 6466 --- bottom_with_polyester_concret_shell WHEN reliner_material = 12 THEN 6467 --- unknown WHEN reliner_material = 13 THEN 6486 --- UP_resin_LED_synthetic_fibre_liner WHEN reliner_material = 14 THEN 6468 --- vinyl_ester_resin_glass_fibre_laminate WHEN reliner_material = 15 THEN 6469 --- vinyl_ester_resin_plastic_felt END,
