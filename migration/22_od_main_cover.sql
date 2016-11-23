c17223db626e5e8cb1c2b5d18cad6d3965bb37a6
Matthias Kuhn <matthias@opengis.ch>
2016-11-23 12:04:39 +0100

    Fix main_cover references in demodata

--- migration/22_od_main_cover.sql
+++ migration/22_od_main_cover.sql
@@ -1,5 +1,10 @@
-UPDATE qgep.od_wastewater_structure ws
-SET fk_main_cover = co.obj_id
-FROM qgep.od_cover co
-LEFT JOIN qgep.od_structure_part sp ON sp.obj_id = co.obj_id
-WHERE sp.fk_wastewater_structure = ws.obj_id
+﻿UPDATE qgep.od_wastewater_structure WS
+SET fk_main_cover = structure_cover.co_obj_id
+FROM (
+  SELECT WS.obj_id AS obj_id, min(CO.obj_id) AS co_obj_id
+  FROM qgep.od_wastewater_structure WS
+  LEFT JOIN qgep.od_structure_part SP ON SP.fk_wastewater_structure = WS.obj_id
+  LEFT JOIN qgep.od_cover CO ON SP.obj_id = CO.obj_id
+  GROUP BY WS.obj_id
+) AS structure_cover
+WHERE structure_cover.obj_id = WS.obj_id;
