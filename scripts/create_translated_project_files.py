a5fb84269857f0b28b1f5a5656395a5eae89dba0
Matthias Kuhn <matthias@opengis.ch>
2019-06-04 14:20:10 +0200

    Scripts to generate ts files

--- scripts/create_translated_project_files.py
+++ scripts/create_translated_project_files.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
-from qgis.core import QgsProject, QgsApplication
+from qgis.core import QgsProject, QgsApplication, QgsSettings
 
 app = QgsApplication([], True)
 app.setPrefixPath("/usr", True)
 app.initQgis()
 def printMessage(message, tag, level):
     print('{tag}: {message}'.format(tag=tag, message=message))
+
+QgsSettings().setValue("locale/userLocale", "de");
+
 app.messageLog().messageReceived.connect(printMessage)
 
 project = QgsProject.instance()
 
-project.read('/usr/src/project/qgep.qgs')
-project.generateTsFile('en')
+project.read('/usr/src/project/qgep_en.qgs')
+project.write()
 
--- scripts/docker/create_translations.sh
+++ scripts/docker/create_translations.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-printf "[pg_qgep]\nhost=postgres\nport=5432\ndbname=gis\nuser=docker\npassword=docker" > ~/.pg_service.conf
-
-wget https://github.com/QGEP/datamodel/releases/download/1.2.0/qgep_v1.2.0_structure_with_value_lists.sql
-
 set -e
-echo "Wait a moment while the database is being loaded"
-while ! psql "service=pg_qgep" -l &> /dev/null
-do
-  printf "."
-  sleep 1
-done
-echo ""
-psql "service=pg_qgep" -f qgep_v1.2.0_structure_with_value_lists.sql
-psql "service=pg_qgep" -c "REFRESH MATERIALIZED VIEW qgep_od.vw_network_node;"
-psql "service=pg_qgep" -c "REFRESH MATERIALIZED VIEW qgep_od.vw_network_segment;"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+${DIR}/initialize_db.sh
 
 xvfb-run python3 /usr/src/scripts/create_ts_files.py
