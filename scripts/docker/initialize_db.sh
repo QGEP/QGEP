53b338cfe981cc51adf31b027a72fd693602d97c
Matthias Kuhn <matthias@opengis.ch>
2019-08-10 13:24:38 +0200

    Fix datamodel_version

--- scripts/docker/initialize_db.sh
+++ scripts/docker/initialize_db.sh
@@ -6,11 +6,7 @@ echo 'Restoring database' && echo -en 'travis_fold:start:restore-db\\r'
 
 printf "[pg_qgep]\nhost=postgres\nport=5432\ndbname=gis\nuser=docker\npassword=docker" > ~/.pg_service.conf
 
-DATAMODEL_VERSION=$(cat ${DIR}/../datamodel_version.txt)
-
-echo ${DIR}
-echo $(ls ${DIR}/..)
-echo $DATAMODEL_VERSION
+DATAMODEL_VERSION=$(cat ${DIR}/../../datamodel_version.txt)
 
 wget https://github.com/QGEP/datamodel/releases/download/${DATAMODEL_VERSION}/qgep_v${DATAMODEL_VERSION}_structure_with_value_lists.sql
 
